b42b69e1372c6483b92b29a57e1888d417834fa7
Pedro Rivas <rmdrobs@gmail.com>
2016-05-29 15:22:04 +0200

    #2, Added automated migration tasks to gradle

---
+++ database/migrations/V1_1__create_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `twitter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `key` varchar(200) NOT NULL,
+  `secret` varchar(200) NOT NULL,
+  `verifier` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
